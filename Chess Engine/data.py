6508fec60fc8e9e9d63c05562111b35092086ba4
Yoshigamer360 <133695386+Yoshigamer360@users.noreply.github.com>
2026-09-23 11:47:00 +0100

    Update data.py
    flipped white and black

--- Chess Engine/data.py
+++ Chess Engine/data.py
@@ -2,13 +2,15 @@
 class Chessboard():
     def __init__(self):
         self.board = [
+            ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br'],
+            ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
             ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr'],
             ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
-            ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
-            ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br']]
+            ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+            ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr']]
 
         self.playerTurn = "white"
